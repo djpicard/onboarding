3f2a0a14065959626db4cc664d0de8a47518bc03
DJ Picard <dj.picard@onepeloton.com>
2021-11-17 16:30:55 -0500

    Added modules as data resources

--- poc.tf
+++ poc.tf
@@ -2,3 +2,10 @@ data "spacelift_stack" "poc" {
     stack_id = "website-poc"
 }
 
+data "spacelift_module" "s3" {
+  module_id = "terraform-aws-s3-peloton"
+}
+
+data "spacelift_module" "cloudfront" {
+  module_id = "terraform-aws-cloudfront-peloton"
+}
